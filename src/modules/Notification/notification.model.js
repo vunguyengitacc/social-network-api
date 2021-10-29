1812272d5a10ce849e005e02a6fd5f236c9a2603
vunguyengitacc <nguyenhoanganhvu.it@gmail.com>
2021-10-29 14:38:56 +0700

    add notification model

---
+++ src/modules/Notification/notification.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const NotificationScheme = mongoose.Schema(
+  {
+    message: String,
+    toId: { type: Schema.Types.ObjectId, ref: "users" },
+    fromId: { type: Schema.Types.ObjectId, ref: "users" },
+    type: Number,
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+    },
+    timestamps: true,
+  }
+);
+
+NotificationScheme.virtual("to", {
+  ref: "users",
+  localField: "toId",
+  foreignField: "_id",
+  justOne: true,
+});
+
+NotificationScheme.virtual("from", {
+  ref: "users",
+  localField: "fromId",
+  foreignField: "_id",
+  justOne: true,
+});
+
+const Notification = mongoose.model("notifications", NotificationScheme);
+
+export default Notification;
