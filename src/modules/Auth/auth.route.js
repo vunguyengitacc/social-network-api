e06cee3045d1c02635d8217795042af294ea7c3b
vunguyengitacc <nguyenhoanganhvu.it@gmail.com>
2021-11-16 11:12:09 +0700

    build: protect login by joi + celebreate

--- src/modules/Auth/auth.route.js
+++ src/modules/Auth/auth.route.js
@@ -2,14 +2,17 @@ import express from "express";
 import passport from "passport";
 import checkToken from "middleware/checkToken.middleware";
 import authController from "./auth.controller";
-import { registerScheme } from "./auth.validate";
 import { celebrate } from "celebrate";
+import authValidate from "./auth.validate";
 
 const AuthRouter = express.Router();
 AuthRouter.route("/me").get(checkToken, authController.getMe);
-AuthRouter.route("/login").post(authController.login);
+AuthRouter.route("/login").post(
+  celebrate(authValidate.loginSchema),
+  authController.login
+);
 AuthRouter.route("/register").post(
-  celebrate(registerScheme),
+  celebrate(authValidate.registerSchema),
   authController.register
 );
 AuthRouter.route("/facebook").get(passport.authenticate("facebook"));
--- src/modules/Auth/auth.validate.js
+++ src/modules/Auth/auth.validate.js
@@ -1,8 +1,22 @@
 import { Joi, Segments } from "celebrate";
-export const registerScheme = {
+const registerSchema = {
   body: Joi.object().keys({
     username: Joi.string().alphanum().min(6).max(30).required(),
     password: Joi.string().alphanum().min(6).max(30).required(),
     fullname: Joi.string().alphanum().min(6).max(30).required(),
   }),
 };
+
+const loginSchema = {
+  body: Joi.object().keys({
+    username: Joi.string().alphanum().min(6).max(30).required(),
+    password: Joi.string().alphanum().min(6).max(30).required(),
+  }),
+};
+
+const authValidate = {
+  registerSchema,
+  loginSchema,
+};
+
+export default authValidate;
